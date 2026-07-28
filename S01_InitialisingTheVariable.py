68d0f169e7fafe6cb8666f530578c087a7997f17
shubham <shubhamlokhande85485@gmail.com>
2026-07-28 16:57:02 +0530

    Modified

--- S01_InitialisingTheVariable.py
+++ S01_InitialisingTheVariable.py
@@ -14,21 +14,25 @@ let dive in it ! Happy Coding! 🚀
 -A variable in Python is a name used to store data or values in a program. The value stored 
 in a variable can be changed during program execution
 
+
 '''
 
 'A.rules to intialise variables in python '
 
 '''
-# 1) never start the variables with numbers - 1234= 
-# 2) never give space in variable - hello world - insted of we can use hello_world or HelloWorld (camel method)
-# 3) write variable in small letter - SPAM= - insted of spam=
-#4) dont use special character as variable like class- as - if - else -
+# 1) Never start the variables with numbers - 1234= 
+# 2) Never give space in variable - hello world - insted of we can use hello_world or HelloWorld (camel method)
+# 3) Write variable in small letter - SPAM= - insted of spam=
+# 4) Dont use special character as variable like class- as - if - else -
+# 5) Always give meaningful name to variable, to specifiy what actual that variable contained
 '''
-
+# Eg
+intro = "Hello World! Welcome"
+print(intro)
 
 'B.Types of initilize variables in python'
 
-#1.one value and many variables 
+'#1.one value and many variables '
 a=b=c=d=100
 print(a)
 print(b)
